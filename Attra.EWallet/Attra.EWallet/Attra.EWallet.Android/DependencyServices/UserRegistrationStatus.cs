e1b7cce8f5cacf7f5c547f628fd9c4409093622d
abhishek.kumar1 <send2abhishek@live.com>
2019-02-04 19:19:08 +0530

    Added ProgressDialog in registration Page

---
+++ Attra.EWallet/Attra.EWallet/Attra.EWallet.Android/DependencyServices/UserRegistrationStatus.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Attra.EWallet.Droid.DependencyServices;
+using Attra.EWallet.Interface;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(UserRegistrationStatus))]
+
+namespace Attra.EWallet.Droid.DependencyServices
+{
+    public class UserRegistrationStatus : RegistertionNotify
+    {
+
+        private ProgressDialog progressDialog = new ProgressDialog(MainActivity.Instance);
+
+
+
+
+
+
+        public void onStartRegistration()
+        {
+
+            progressDialog.SetMessage("Attempting to Register User");
+            progressDialog.SetTitle("Please Wait...");
+            progressDialog.SetCancelable(false);
+            progressDialog.Show();
+        }
+        public void onCompleteRegistration()
+        {
+
+            progressDialog.Dismiss();
+
+
+        }
+
+        public void onRegistrationSucces(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context, msg, ToastLength.Short).Show();
+        }
+
+        public void onRegistrationFail(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context, msg, ToastLength.Short).Show();
+        }
+    }
+}
