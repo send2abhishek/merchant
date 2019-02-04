e99820088aeaa0592c8fa2c02df2ff43dab8c65f
AutoPilot Admin <yugandharreddy.singam@attra.com.au>
2019-02-04 15:26:19 +0530

    Restructured the project for better maintainability.

---
+++ Attra.EWallet/Attra.EWallet.DAL/Models/LoginUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attra.EWallet.DAL.Models
+{
+   public class LoginUser
+    {
+        
+            //public string MobileNo { get; set; }
+            public string email { get; set; }
+            public string password { get; set; }
+       
+    }
+}
