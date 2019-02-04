00c10adcd44836264f73463af767bb151c25787d
abhishek.kumar1 <send2abhishek@live.com>
2019-02-04 19:27:11 +0530

    created Interface for Progress Dialog Implementation

---
+++ Attra.EWallet/Attra.EWallet/Attra.EWallet/Interface/RegistertionNotify.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Attra.EWallet.Interface
+{
+    public interface RegistertionNotify
+    {
+
+        void onStartRegistration();
+        void onCompleteRegistration();
+        void onRegistrationSucces(String msg);
+        void onRegistrationFail(String msg);
+    }
+}
