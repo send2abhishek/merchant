9d44cb092ffc3bb94860ee1eb9681fe568bb0e56
abhishek.kumar1 <send2abhishek@live.com>
2019-02-05 21:58:12 +0530

    Intigrated registration Services

---
+++ EWallet.Merchant/EWallet.Merchant.DAL/Models/MerchantRegistration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EWallet.Merchant.DAL.Models
+{
+    public class MerchantRegistration
+    {
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string location { get; set; }
+        public string type { get; set; }
+        public string mobile { get; set; }
+        public string realm { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public bool emailVerified { get; set; }
+
+    }
+}
---
+++ EWallet.Merchant/EWallet.Merchant.DAL/Services/ApiServices.cs
@@ -0,0 +1,34 @@
+﻿using EWallet.Merchant.DAL.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWallet.Merchant.DAL.Services
+{
+    public class ApiServices
+    {
+        public async Task<bool> RegisterUser(string fName, string mobileNbr, int empid, string Email)
+        {
+
+            MerchantRegistration registration = new MerchantRegistration()
+            {
+
+
+
+
+            };
+
+
+            // httpClient is used for Couminicate With the server
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(registration);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/merchants", content);
+            return resposne.IsSuccessStatusCode;
+        }
+    }
+}
+
