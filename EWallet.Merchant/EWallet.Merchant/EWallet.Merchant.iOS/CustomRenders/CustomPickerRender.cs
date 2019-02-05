82e86ca11afe60983cd071d364b134f23926b589
abhishek.kumar1 <send2abhishek@live.com>
2019-02-05 21:55:55 +0530

    Added custom render

---
+++ EWallet.Merchant/EWallet.Merchant/EWallet.Merchant.iOS/CustomRenders/CustomPickerRender.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using EWallet.Merchant.CustomControls;
+using EWallet.Merchant.Droid.CustomRenders;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRender))]
+
+namespace EWallet.Merchant.Droid.CustomRenders
+{
+    public class CustomPickerRender : PickerRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            SetControlStyle();
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            SetControlStyle();
+        }
+
+        private void SetControlStyle()
+        {
+            if (Control != null)
+            {
+                var imgDropDownArrow = UIImage.FromFile("ddarrow.png");
+                Control.RightViewMode = UITextFieldViewMode.Always;
+                Control.RightView = new UIImageView(imgDropDownArrow);
+            }
+        }
+
+    }
+}
