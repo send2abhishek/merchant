6656f4a29310be96a09507c3ba4fdd03fc421916
abhishek.kumar1 <send2abhishek@live.com>
2019-02-05 22:06:23 +0530

    Added APi Service

--- Attra.EWallet/Attra.EWallet.DAL/Services/ApiServices.cs
+++ Attra.EWallet/Attra.EWallet.DAL/Services/ApiServices.cs
@@ -11,7 +11,7 @@ namespace Attra.EWallet.DAL.Services
   public class ApiServices
     {
 
-        public async Task<bool> RegisterUser(string fName, string mobileNbr, int empid, string Email)
+        public async Task<bool> RegisterUser(string fName, string mobileNbr, int empid, string Email,string Passsword)
         {
 
             PayerRegistration registration = new PayerRegistration()
@@ -22,9 +22,9 @@ namespace Attra.EWallet.DAL.Services
                 mobile = mobileNbr,
                 empId = empid,
                 realm = "Payer",
-                username = empid.ToString(),
+                username = mobileNbr.ToString(),
                 email = Email,
-                password = "Default_pass",
+                password = Passsword,
                 emailVerified = true,
 
 
@@ -36,6 +36,58 @@ namespace Attra.EWallet.DAL.Services
             var json = JsonConvert.SerializeObject(registration);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
             var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/payers", content);
+            return resposne.IsSuccessStatusCode;
+        }
+
+
+        public async Task<OtpResponse> ValidateOtp(double Otp, string Email, string phnbr)
+        {
+
+            OtpValidate validate=new OtpValidate()
+            {
+
+                OTP= Otp,
+                email= Email,
+                mobileNumber= phnbr
+
+
+            };
+
+
+            // httpClient is used for Couminicate With the server
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(validate);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/payers", content);
+            var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/payers/confirmOTP", content);
+
+            var jsonString = await resposne.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<OtpResponse>(jsonString);
+        }
+
+
+        public async Task<bool> ReSendOtpOtp(string Email, string phnbr)
+        {
+
+            ReSendOtp validate = new ReSendOtp()
+            {
+
+                
+                email = Email,
+                mobileNumber = phnbr
+
+
+            };
+
+
+            // httpClient is used for Couminicate With the server
+            var httpClient = new HttpClient();
+            var json = JsonConvert.SerializeObject(validate);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            //var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/payers", content);
+            var resposne = await httpClient.PostAsync("http://ec2-13-127-1-106.ap-south-1.compute.amazonaws.com:4000/api/payers/resendOTP", content);
+
+
             return resposne.IsSuccessStatusCode;
         }
     }
