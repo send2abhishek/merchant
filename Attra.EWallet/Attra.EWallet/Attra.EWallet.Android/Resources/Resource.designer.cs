7bda2cfe0fcfefc4e05786de6574750c83453cd7
abhishek.kumar1 <send2abhishek@live.com>
2019-02-05 22:03:36 +0530

    Added Image in resource directory

--- Attra.EWallet/Attra.EWallet/Attra.EWallet.Android/Resources/Resource.designer.cs
+++ Attra.EWallet/Attra.EWallet/Attra.EWallet.Android/Resources/Resource.designer.cs
@@ -2348,26 +2348,26 @@ namespace Attra.EWallet.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f020147
-			public const int avd_hide_password_1 = 2130837831;
+			// aapt resource value: 0x7f02014d
+			public const int avd_hide_password_1 = 2130837837;
 			
-			// aapt resource value: 0x7f020148
-			public const int avd_hide_password_2 = 2130837832;
+			// aapt resource value: 0x7f02014e
+			public const int avd_hide_password_2 = 2130837838;
 			
-			// aapt resource value: 0x7f020149
-			public const int avd_hide_password_3 = 2130837833;
+			// aapt resource value: 0x7f02014f
+			public const int avd_hide_password_3 = 2130837839;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f02014a
-			public const int avd_show_password_1 = 2130837834;
+			// aapt resource value: 0x7f020150
+			public const int avd_show_password_1 = 2130837840;
 			
-			// aapt resource value: 0x7f02014b
-			public const int avd_show_password_2 = 2130837835;
+			// aapt resource value: 0x7f020151
+			public const int avd_show_password_2 = 2130837841;
 			
-			// aapt resource value: 0x7f02014c
-			public const int avd_show_password_3 = 2130837836;
+			// aapt resource value: 0x7f020152
+			public const int avd_show_password_3 = 2130837842;
 			
 			// aapt resource value: 0x7f020057
 			public const int background_screen_two = 2130837591;
@@ -2973,11 +2973,7 @@ namespace Attra.EWallet.Droid
 			public const int iPhoneX = 2130837791;
 			
 			// aapt resource value: 0x7f020120
-<<<<<<< HEAD
 			public const int LeftArrow = 2130837792;
-=======
-			public const int lgscreen_bg = 2130837792;
->>>>>>> 4d51a25222868ec144393f5f195354a9b7ac8a55
 			
 			// aapt resource value: 0x7f020121
 			public const int money_bag = 2130837793;
@@ -3069,11 +3065,11 @@ namespace Attra.EWallet.Droid
 			// aapt resource value: 0x7f02013e
 			public const int notification_icon_background = 2130837822;
 			
-			// aapt resource value: 0x7f020145
-			public const int notification_template_icon_bg = 2130837829;
+			// aapt resource value: 0x7f02014b
+			public const int notification_template_icon_bg = 2130837835;
 			
-			// aapt resource value: 0x7f020146
-			public const int notification_template_icon_low_bg = 2130837830;
+			// aapt resource value: 0x7f02014c
+			public const int notification_template_icon_low_bg = 2130837836;
 			
 			// aapt resource value: 0x7f02013f
 			public const int notification_tile_bg = 2130837823;
@@ -3082,16 +3078,34 @@ namespace Attra.EWallet.Droid
 			public const int notify_panel_notification_icon_bg = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int Search = 2130837825;
+			public const int payee_emp_name = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int SendArrow = 2130837826;
+			public const int payee_password = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int tooltip_frame_dark = 2130837827;
+			public const int payee_reg_img = 2130837827;
 			
 			// aapt resource value: 0x7f020144
-			public const int tooltip_frame_light = 2130837828;
+			public const int payee_reg_mail = 2130837828;
+			
+			// aapt resource value: 0x7f020145
+			public const int payee_reg_name = 2130837829;
+			
+			// aapt resource value: 0x7f020146
+			public const int payee_reg_phone = 2130837830;
+			
+			// aapt resource value: 0x7f020147
+			public const int Search = 2130837831;
+			
+			// aapt resource value: 0x7f020148
+			public const int SendArrow = 2130837832;
+			
+			// aapt resource value: 0x7f020149
+			public const int tooltip_frame_dark = 2130837833;
+			
+			// aapt resource value: 0x7f02014a
+			public const int tooltip_frame_light = 2130837834;
 			
 			static Drawable()
 			{
